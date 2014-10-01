0684212eae5e9b6be4bb46fe30c44ae6660a77fd
Bravocado <bravocado.project@gmail.com>
2014-10-01 13:11:40 +0700

    adding meta

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,3 +1,3 @@
 var gulp = require('gulp');
 
-gulp.task('build', ['clean', 'templates', 'sass', 'sass:compressed']);
+gulp.task('build', ['clean', 'templates', 'sass', 'sass:compressed', 'critical']);

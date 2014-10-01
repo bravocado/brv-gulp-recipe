d5ed60af780f4b22dd66cb53f08513478d209606
Bravocado <bravocado.project@gmail.com>
2014-10-01 12:53:49 +0700

    debug async css [3]

--- gulp/tasks/copystyles.js
+++ gulp/tasks/copystyles.js
@@ -5,7 +5,7 @@ var gulp = require('gulp'),
 
 // Copy our site styles to a site.css file
 // for async loading later
-gulp.task('copystyles', ['sass'] , function () {
+gulp.task('copystyles', ['sass', 'sass:compressed'] , function () {
   return gulp.src(config.src)
     .pipe(rename({
       basename: "critical"
